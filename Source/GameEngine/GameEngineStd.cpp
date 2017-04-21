daadb6cf1796c98f32793e2d7725b46e15a67ae9
Enrique Gonzalez Rodriguez <enriquegr84@hotmail.es>
2017-04-21 12:30:22 +0100

    project setting changes

--- Source/GameEngine/GameEngineStd.cpp
+++ Source/GameEngine/GameEngineStd.cpp
@@ -8,24 +8,22 @@
 
 #if defined(_M_IX86)
 	#if defined(_DEBUG)
-		#pragma comment(lib, "bullet3collision_debug.lib")
-		#pragma comment(lib, "bullet3dynamics_debug.lib")
 		#pragma comment(lib, "bulletcollision_debug.lib")
 		#pragma comment(lib, "bulletdynamics_debug.lib")
 		#pragma comment(lib, "linearmath_debug.lib")
 	#else
-		#pragma comment(lib, "bullet3collision.lib")
-		#pragma comment(lib, "bullet3dynamics.lib")
+		#pragma comment(lib, "bulletcollision.lib")
+		#pragma comment(lib, "bulletdynamics.lib")
 		#pragma comment(lib, "linearmath.lib")
 	#endif
 #elif defined(_M_X64)
 	#if defined(_DEBUG)
-		#pragma comment(lib, "bullet3collision_x64__debug.lib")
-		#pragma comment(lib, "bullet3dynamics_x64__debug.lib")
+		#pragma comment(lib, "bulletcollision_x64__debug.lib")
+		#pragma comment(lib, "bulletdynamics_x64__debug.lib")
 		#pragma comment(lib, "linearmath_x64_debug.lib")
 	#else
-		#pragma comment(lib, "bullet3collision_x64.lib")
-		#pragma comment(lib, "bullet3dynamics_x64.lib")
+		#pragma comment(lib, "bulletcollision_x64.lib")
+		#pragma comment(lib, "bulletdynamics_x64.lib")
 		#pragma comment(lib, "linearmath_x64.lib")
 	#endif
 #else
