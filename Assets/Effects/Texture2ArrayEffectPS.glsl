37de4dee95a8da4fd322eb019a963f4814cc7783
Enrique Gonzalez Rodriguez <enriquegr84@hotmail.es>
2018-12-28 18:30:45 +0000

    readding

---
+++ Assets/Effects/Texture2ArrayEffectPS.glsl
@@ -0,0 +1,19 @@
+// David Eberly, Geometric Tools, Redmond WA 98052
+// Copyright (c) 1998-2017
+// Distributed under the Boost Software License, Version 1.0.
+// http://www.boost.org/LICENSE_1_0.txt
+// http://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+// File Version: 3.0.0 (2016/06/19)
+
+uniform sampler2D baseSampler;
+
+layout(location = 0) in vec2 vertexTCoord;
+layout(location = 0) out vec4 pixelColor;
+
+void main()
+{
+	// Sample first 2D texture array.
+    vec4 tcd;
+    tcd.xyz = vec3(vertexTCoord, 0);
+    pixelColor0 = texture(baseSampler, tcd.xyz);
+}
